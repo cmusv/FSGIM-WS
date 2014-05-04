7c13a59f528ee5824dc55d34c7314966d89db8dc
Surya Kiran <suryakiranl@gmail.com>
2014-05-03 22:33:42 -0700

    Data transfer objects to send info to UI

---
+++ src/edu/cmu/sv/fsgim/business/dto/Query.java
@@ -0,0 +1,64 @@
+package edu.cmu.sv.fsgim.business.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "query")
+public class Query extends BaseDTO {
+	private String queryClassification;
+	private String queryName;
+	private String queryString;
+	private String modelVersion;
+	private String modelName;
+
+	public String getQueryClassification() {
+		return queryClassification;
+	}
+
+	public void setQueryClassification(String queryClassification) {
+		this.queryClassification = queryClassification;
+	}
+
+	public String getQueryName() {
+		return queryName;
+	}
+
+	public void setQueryName(String queryName) {
+		this.queryName = queryName;
+	}
+
+	public String getQueryString() {
+		return queryString;
+	}
+
+	public void setQueryString(String queryString) {
+		this.queryString = queryString;
+	}
+
+	public String getModelVersion() {
+		return modelVersion;
+	}
+
+	public void setModelVersion(String modelVersion) {
+		this.modelVersion = modelVersion;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	@Override
+	public String toString() {
+		return "Query [queryClassification=" + queryClassification
+				+ ", queryName=" + queryName + ", queryString=" + queryString
+				+ ", modelVersion=" + modelVersion + ", modelName=" + modelName
+				+ ", getId()=" + getId() + ", getModifiedBy()="
+				+ getModifiedBy() + ", getModifiedTime()=" + getModifiedTime()
+				+ ", getCreatedBy()=" + getCreatedBy() + ", getCreatedTime()="
+				+ getCreatedTime() + "]";
+	}
+
+}
