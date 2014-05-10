6ee5de82174f4e9eb28b9eb87e07f3f69a79041c
Surya Kiran <suryakiranl@gmail.com>
2014-05-09 19:37:56 -0700

    Final Working Copy
    1. Header Div is in center of page.
    2. Home and Logout links work from all pages

--- war/js/fsgim.js
+++ war/js/fsgim.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
 	var userId = localStorage.getItem("userId");
+	var sURL = document.URL;
+	var baseURL = sURL.substring(0, sURL.indexOf('/', 7));
+	
 	if(!userId) {
 		console.log("No logged in user. Redirecting to login page...");
-		
-		var sURL = document.URL;
-		var baseURL = sURL.substring(0, sURL.indexOf('/', 7));
-
 		window.location.replace(baseURL + "/login.html");
 	} else {
 		// Add a footer for users to use.
 		var userName = localStorage.getItem("userName");
-		var divHtml = '<div id="footerDiv">' +
-		'<span>Welcome ' + userName + '!!</span>' +
-		'<a id="logout" href="login.html">Logout</a> '+
+		var spaces = '&nbsp;&nbsp;&nbsp;&nbsp;';
+		var divHtml = '<div id="headerDiv"><br/><br/>' +
+		'<b>Welcome ' + userName + ' !</b>' + spaces + 
+		'<a id="home" href="' + baseURL + '/index.html">Home</a>'+ spaces + 
+		'<a id="logout" href="' + baseURL + '/login.html">Logout</a> '+
 		'</div>';
 		$('body').prepend(divHtml);
 		
