5814bc04361e6d07ea44eef28c69754635335997
Surya Kiran <suryakiranl@gmail.com>
2014-05-10 15:48:44 -0700

    Added new method to validate empty input values. It also displays notification message with the field name which can be passed as a parameter

--- war/js/fsgim.js
+++ war/js/fsgim.js
@@ -1,26 +1,38 @@
-$(document).ready(function() {
-	var userId = localStorage.getItem("userId");
-	var sURL = document.URL;
-	var baseURL = sURL.substring(0, sURL.indexOf('/', 7));
-	
-	if(!userId) {
-		console.log("No logged in user. Redirecting to login page...");
-		window.location.replace(baseURL + "/login.html");
-	} else {
-		// Add a footer for users to use.
-		var userName = localStorage.getItem("userName");
-		var spaces = '&nbsp;&nbsp;&nbsp;&nbsp;';
-		var divHtml = '<div id="headerDiv"><br/><br/>' +
-		'<b>Welcome ' + userName + ' !</b>' + spaces + 
-		'<a id="home" href="' + baseURL + '/index.html">Home</a>'+ spaces + 
-		'<a id="logout" href="' + baseURL + '/login.html">Logout</a> '+
-		'</div>';
-		$('body').prepend(divHtml);
-		
-		// Add onclick listener for logout link
-		$('#logout').click(function() {
-			localStorage.removeItem('userId');
-			localStorage.removeItem('userName');
+$(document).ready(
+		function() {
+			var userId = localStorage.getItem("userId");
+			var sURL = document.URL;
+			var baseURL = sURL.substring(0, sURL.indexOf('/', 7));
+
+			if (!userId) {
+				console.log("No logged in user. Redirecting to login page...");
+				window.location.replace(baseURL + "/login.html");
+			} else {
+				// Add a footer for users to use.
+				var userName = localStorage.getItem("userName");
+				var spaces = '&nbsp;&nbsp;&nbsp;&nbsp;';
+				var divHtml = '<div id="headerDiv"><br/><br/>' + '<b>Welcome '
+						+ userName + ' !</b>' + spaces + '<a id="home" href="'
+						+ baseURL + '/index.html">Home</a>' + spaces
+						+ '<a id="logout" href="' + baseURL
+						+ '/login.html">Logout</a> ' + '</div>';
+				$('body').prepend(divHtml);
+
+				// Add onclick listener for logout link
+				$('#logout').click(function() {
+					localStorage.removeItem('userId');
+					localStorage.removeItem('userName');
+				});
+			}
 		});
+
+// Utility method to check if a field has empty value
+// and shows a notification.
+function validateEmptyString(value, fieldName) {
+	if ("" == value.trim()) {
+		showNotification(fieldName + " is mandatory.");
+		return false;
 	}
-});
+	
+	return true;
+}
