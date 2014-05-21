71cff7778ed0b3438a0c74dcf16233115d0fa1f9
Surya Kiran <suryakiranl@gmail.com>
2014-05-20 21:15:50 -0700

    Updating JS to support GET, POST and DELETE
    Support GET POST and DELETE for Versions

--- war/js/versions.js
+++ war/js/versions.js
@@ -1,49 +1,25 @@
-// Validate for good data
-function validatePage() {
-	var userName = $("#userName").val();
-	var password = $("#password").val();
-	var passwordRe = $("#passwordRe").val();
-
-	if (!(validateEmptyString(userName, 'User name') && validateEmptyString(
-			password, 'Password'))) {
-		return false;
-	}
-
-	if (password != passwordRe) {
-		showNotification("Error: Passwords do not match. Please try again");
-		return false;
-	}
-	
-	return true;
-}
-
 // Convert the data on the page to JSON
 function dataToJSON() {
 	return JSON.stringify({
-		userName : $("#userName").val(),
-		password : $("#password").val()
+		versionNumber : $("#versionNumber").val(),
+		description : $("#description").val()
 	});
 }
 
 // Function to clear all the fields on the page,
 // to get the page ready for the next query.
 function clearAllFields() {
-	showNotification("User saved successfully");
-	$("#userName").val("");
-	$("#password").val("");
-	$("#passwordRe").val("");
+	$("#versionNumber").val("");
+	$("#description").val("");
 }
 
 function onLoadCreatePage() {
 	$("#addBtn").click(
 			function() {
-				if (!validatePage()) {
-					return false;
-				}
 				showNotification("Saving information. Please wait ... ");
 
 				request = $.ajax({
-					url : "/rest/users",
+					url : "/rest/versions",
 					type : "POST",
 					contentType : 'application/json',
 					dataType : 'json',
@@ -53,6 +29,7 @@ function onLoadCreatePage() {
 				// callback handler that will be called on success
 				request.done(function(response, textStatus, jqXHR) {
 					// log a message to the console
+					showNotification("Version saved successfully");
 					clearAllFields();
 				});
 
@@ -69,7 +46,7 @@ function onLoadCreatePage() {
 
 function onLoadShowPage() {
 	request = $.ajax({
-		url : "/rest/users",
+		url : "/rest/versions",
 		type : "GET",
 		contentType : 'application/json',
 		dataType : 'json'
@@ -82,23 +59,27 @@ function onLoadShowPage() {
 				console.log("Response obtained:: " + response);
 
 				// Populate the results table on the page.
-				$("#usersListingTbl")
+				$("#versionsListingTbl")
 						.append(
 								"<tr><td align='center'>"
-										+ "<b>Following is a list of all available Users</b></td></tr>");
-				var headerRow = "<tr>" + "<td><b>User Name</b></td>" + "</tr>";
-				$(headerRow).appendTo("#usersListingTbl > tbody");
+										+ "<b>Following is a list of all available Versions</b></td></tr>");
+				var headerRow = "<tr>" 
+					+ "<td><b>Version Number</b></td>" 
+					+ "<td><b>Description</b></td>"
+					+ "</tr>";
+				$(headerRow).appendTo("#versionsListingTbl > tbody");
 				$
 						.each(
 								response,
-								function(i, user) {
+								function(i, version) {
 									var deleteButton = "<input type='button' value='Delete' onclick='performDelete("
-											+ user.id + ")' />";
-									var queryRow = "<tr id ='" + user.id + "'>"
-											+ "<td>" + user.userName + "</td>"
+											+ version.id + ")' />";
+									var queryRow = "<tr id ='" + version.id + "'>"
+											+ "<td>" + version.versionNumber + "</td>"
+											+ "<td>" + version.description + "</td>"
 											+ "<td>" + deleteButton + "</td>"
 											+ "</tr>";
-									$("#usersListingTbl").append(queryRow);
+									$("#versionsListingTbl").append(queryRow);
 								});
 			});
 
@@ -109,12 +90,12 @@ function onLoadShowPage() {
 	});
 }
 
-function performDelete(userId) {
-	console.log("User ID = " + userId);
-	localStorage.setItem('delUserId', userId);
+function performDelete(versionId) {
+	console.log("Version ID = " + versionId);
+	localStorage.setItem('delVersionId', versionId);
 
 	req = $.ajax({
-		url : "/rest/users/" + userId,
+		url : "/rest/versions/" + versionId,
 		type : "DELETE",
 		contentType : 'application/json',
 		dataType : 'json'
@@ -122,11 +103,11 @@ function performDelete(userId) {
 
 	// callback handler that will be called on success
 	req.done(function(response, textStatus, jqXHR) {
-		showNotification("User deleted successfully.");
-		var uId = localStorage.getItem('delUserId');
-		$('table#usersListingTbl tr#' + uId).remove();
+		showNotification("Version deleted successfully.");
+		var uId = localStorage.getItem('delVersionId');
+		$('table#versionsListingTbl tr#' + uId).remove();
 		console.log('Row with id ' + uId + ' removed from table');
-		localStorage.removeItem('delUserId');
+		localStorage.removeItem('delVersionId');
 	});
 
 	// callback handler that will be called on failure
