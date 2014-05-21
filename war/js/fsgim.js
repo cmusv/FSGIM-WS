05386b9cb480a1cd929b112b0aa00d345b0902c1
Surya Kiran <suryakiranl@gmail.com>
2014-05-21 00:05:14 -0700

    Generic methods to load models and versions
    2 methods to load model names and model version using AJAX calls in
    versions and queries pages.

--- war/js/fsgim.js
+++ war/js/fsgim.js
@@ -42,6 +42,7 @@ function validateEmptyString(value, fieldName) {
 }
 
 function populateModelNames(comboBoxId) {
+	showNotification("Loading model names. Please wait ... ");
 	console.log("Getting model names to populate in combo box: " + comboBoxId);
 	request = $.ajax({
 		url : "/rest/models",
@@ -61,6 +62,47 @@ function populateModelNames(comboBoxId) {
 				function(i, model) {
 					$("#" + comboBoxId).append($('<option></option>').val(model.modelName).html(model.modelName));
 				});
+		
+		callBackAfterPopulateModelNames(comboBoxId);
+	});
+
+	// callback handler that will be called on failure
+	request.fail(function(jqXHR, textStatus, errorThrown) {
+		// log the error to the console
+		console.log("The following error occured: " + textStatus, errorThrown);
+	});
+}
+
+function populateModelVersions(modelNameComboBoxId, modelVersionComboBoxId) {
+	showNotification("Loading model versions. Please wait ... ");
+	console.log("Getting model versions to populate in combo box: " + modelVersionComboBoxId);
+	request = $.ajax({
+		url : "/rest/versions",
+		type : "GET",
+		contentType : 'application/json',
+		dataType : 'json'
+	});
+
+	// callback handler that will be called on success
+	request.done(
+		function(response, textStatus, jqXHR) {
+		// log a message to the console
+		console.log("Response obtained:: " + response);
+
+		// Empty the list before adding new versions
+		$("#" + modelVersionComboBoxId).empty();
+		
+		var modelName = $("#" + modelNameComboBoxId).find(":selected").text();
+		console.log("Selected modelName = " + modelName);
+		// Populate the results table on the page.
+		$.each(response, 
+				function(i, version) {
+					console.log("modelName=" + version.modelName + ", versionNumber=" + version.versionNumber);
+					if(version.modelName == modelName) {
+						$("#" + modelVersionComboBoxId).append($('<option></option>').val(version.versionNumber).html(version.versionNumber));
+					}
+				});
+		callBackAfterPopulateModelVersions(modelNameComboBoxId, modelVersionComboBoxId);
 	});
 
 	// callback handler that will be called on failure
